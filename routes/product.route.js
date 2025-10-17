84971c4a1164c68c9bca86bb6650a9e75fdf9231
Sayeed Munees <sayeedmunees0@gmail.com>
2025-10-17 20:50:04 +0530

    Done with routes and controllers

--- routes/product.route.js
+++ routes/product.route.js
@@ -1,5 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const Product = require("./models/product.model.js");
+const {
+  getProducts,
+  getProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} = require("../controllers/product.controller.js");
 
-router.get("/", );
+router.get("/", getProducts);
+router.get("/:id", getProduct);
+
+// create a product
+router.post("/", createProduct);
+
+// update a product
+router.put("/:id", updateProduct);
+
+// delete a product
+router.delete("/:id", deleteProduct);
+
+module.exports = router;
