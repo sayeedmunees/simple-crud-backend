00762b62da23cbb18755a4d2811346af49a42880
Sayeed Munees <sayeedmunees0@gmail.com>
2025-10-17 19:26:31 +0530

    Updated getting products

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const Product = require("./models/product.model.js");
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World from API Server");
 });
 
-api.post("/api/products", (req, res) => {
-  console.log(req.body);
-  res.send(req.body);
+app.get("/api/products/", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.get("/api/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const products = await Product.findById(id);
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.post("/api/products", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 mongoose
